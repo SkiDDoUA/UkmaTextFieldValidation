1dcc25dc89b093c8c9740928b6529f73c48ffe95
Anton <antonlena516@gmail.com>
2022-12-12 10:51:41 +0200

    Initial Commit

---
+++ UkmaTextFieldValidation/UkmaTextFieldValidation.h
@@ -0,0 +1,18 @@
+//
+//  UkmaTextFieldValidation.h
+//  UkmaTextFieldValidation
+//
+//  Created by Anton Kolesnikov on 12.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UkmaTextFieldValidation.
+FOUNDATION_EXPORT double UkmaTextFieldValidationVersionNumber;
+
+//! Project version string for UkmaTextFieldValidation.
+FOUNDATION_EXPORT const unsigned char UkmaTextFieldValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UkmaTextFieldValidation/PublicHeader.h>
+
+
